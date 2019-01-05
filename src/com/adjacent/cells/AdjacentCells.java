602081c005985c8d70d070dfdaed3c8f9d389f07
João Morais [1160613] <1160613@isep.ipp.pt>
2019-01-05 00:25:29 +0000

    implemented call to flood fill algorithm

--- src/com/adjacent/cells/AdjacentCells.java
+++ src/com/adjacent/cells/AdjacentCells.java
@@ -2,23 +2,42 @@ package com.adjacent.cells;
 
 import java.util.List;
 
+/**
+ * AdjacentCells class
+ * <p>
+ * Queue based implementation of Flood Fill Algorithm.
+ */
 public class AdjacentCells {
 
-    private int lines;
-    private int columns;
+    /**
+     * Number of Lines
+     */
+    private int numLines;
+    /**
+     * Number of Columns
+     */
+    private int numColumns;
     private int[][] matrix;
-    private List<String> adjacentCells;
 
     AdjacentCells(int lines, int columns, int[][] matrix) {
-        this.lines = lines;
-        this.columns = columns;
+        this.numLines = lines;
+        this.numColumns = columns;
         this.matrix = matrix;
     }
 
+    List<Coordinates> floodFill() {
+
+        // execute floodFill
+        FloodFill floodFill = new FloodFill();
+
+        // return solution
+        return floodFill.getSolution(matrix, 1, 0);
+    }
+
     void printing() {
-        for (int x = 0; x < this.lines; x++) {
-            for (int y = 0; y < this.columns; y++) {
-                System.out.print(matrix[y][x] + ", ");
+        for (int x = 0; x < this.numLines; x++) {
+            for (int y = 0; y < this.numColumns; y++) {
+                System.out.print(matrix[x][y] + ", ");
             }
             System.out.println();
         }
