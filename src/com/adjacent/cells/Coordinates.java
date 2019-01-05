433b4632eea1f2f033bac6424df2bfc9a1204c49
João Morais [1160613] <1160613@isep.ipp.pt>
2019-01-05 00:26:10 +0000

    created Coordinates class in order to easily store two coordinates of a point (X, Y)

--- src/com/adjacent/cells/Coordinates.java
+++ src/com/adjacent/cells/Coordinates.java
@@ -4,4 +4,20 @@ package com.adjacent.cells;
  * Helper class to store values of X and Y coordinates
  */
 public class Coordinates {
+
+    private int x;
+    private int y;
+
+    Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    int getX() {
+        return this.x;
+    }
+
+    int getY() {
+        return this.y;
+    }
 }
